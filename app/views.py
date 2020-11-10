d0df421e2274e26dc3f76c6d60654a7e38fb7693
kaustubhgupta <kaustubhgupta1828@gmail.com>
2020-11-10 23:53:02 +0530

    All things Done in Website

--- app/views.py
+++ app/views.py
@@ -14,6 +14,14 @@ def page_not_found(error):
 def home():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 
 @app.route('/', methods=['POST'])
 def upload_file():
